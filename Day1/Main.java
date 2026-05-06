47de83dc626232fd8db1f993ca0f23b0df0932a3
sachin555-lab <sachintailor711@gmail.com>
2026-05-07 01:11:24 +0530

    first commit

---
+++ Day1/Main.java
@@ -0,0 +1,60 @@
+
+
+// public class Main {
+//     public static void main(String[] args){
+//         System.out.println("hello world!");
+//     }
+
+// }
+
+
+
+// public class Main{
+//     public static void main(String args[]){
+//         System.out.print("sachin ");
+//         System.out.print("how ");
+//         System.out.print("r u");
+
+//     }
+// }
+
+
+
+
+// public class Main{
+//     public static void main(String args[]){
+//         System.out.print("sachin \n");
+//         System.out.print("how \n");
+//         System.out.print("r u");
+
+//     }
+// }
+
+
+
+
+// public class Main{
+//     public static void main(String args[]){
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+
+//     }
+// }
+
+
+
+
+public class Main{
+    public static void main(String agrs []){
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+
+        String name = "king G";
+        System.out.println(name);
+
+    }
+}
---
+++ Day1/tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+public class Main{
+//     public static void main(String args[]){
+//         System.out.print("sachin ");
+//         System.out.print("how ");
+//         System.out.print("r u");
+
+//     }
+// }
+
+
+
+// public class Main{
+//     public static void main(String args[]){
+//         System.out.print("sachin \n");
+//         System.out.print("how \n");
+//         System.out.print("r u");
+
+//     }
+// }
