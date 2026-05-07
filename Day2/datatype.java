976a9d0bf4f68324f7c046f06a556f89ce8c40a0
sachin555-lab <sachintailor711@gmail.com>
2026-05-08 01:26:19 +0530

    Refactor datatype and input classes to uncomment and enable basic operations

--- Day2/datatype.java
+++ Day2/datatype.java
@@ -1,17 +1,17 @@
-// public class datatype{
-//     public static void main(String args[]){
-//         byte b= 8;
-//         System.out.println(b);
-//         char ch = 'a';
-//         // char ch = 'ab';   // error is occouring
-//         System.out.println(ch);
-//         boolean var = false;
-//         float price = 2.5f;
-//         int number = 25;
+public class datatype{
+    public static void main(String args[]){
+        byte b= 8;
+        System.out.println(b);
+        char ch = 'a';
+        // char ch = 'ab';   // error is occouring
+        System.out.println(ch);
+        boolean var = false;
+        float price = 2.5f;
+        int number = 25;
 
-//     }
+    }
 
-// }
+}
 
 
 
--- Day2/input.java
+++ Day2/input.java
@@ -1,105 +1,105 @@
 
 
-// import java.util.*;
+import java.util.*;
 
-// public class input{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);   // input lene ke liye hame scanner ki jarurat hoti hai.
-//         String input = sc.next();
-//         System.out.println(input);
-//     }
-// }
-
-
-
-
-
-// sum of a & b --------------------------------------------
-
-// import java.util.*;
-
-// public class input{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-//         int sum = a + b;
-//         System.out.println(sum);
-
-//     }
-// }
+public class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);   // input lene ke liye hame scanner ki jarurat hoti hai.
+        String input = sc.next();
+        System.out.println(input);
+    }
+}
 
 
 
 
 
-// product of a & b-------------------------------------------
+sum of a & b --------------------------------------------
 
+import java.util.*;
 
-// import java.util.*;
+public class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
 
-// public class input{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-//         int product = a*b;
-//         System.out.println(product);
-
-//     }
-// }
-
-
-
-
-// Area of circle ------------------------------------------------
-
-// import java.util.*;
-
-// public class input{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         float rad = sc.nextFloat();
-//         float area = 3.14f*rad*rad;    // java mein float value ke aage f lagta hai nahi vo double ho jaati hai ....
-//         System.out.println(area);
-
-//     }
-// }
+    }
+}
 
 
 
 
-// conversion------------------------------------------------------
 
-// import java.util.*;
-
-// public class input{
-//     public static void main(String args[]){
-//        int a = 25;
-//        long b =a;
-
-//     //    long a = 25;
-//     //    int b =a;
-
-//        System.out.println(b);
-
-//     }
-// }
+product of a & b-------------------------------------------
 
 
+import java.util.*;
 
-// import java.util.*;
+public class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a*b;
+        System.out.println(product);
 
-// public class input{
-//     public static void main(String args[]){
-//         Scanner sc = new Scanner(System.in);
-//         // int number = sc.nextFloat();
-//         float number = sc.nextInt();
+    }
+}
+
+
+
+
+Area of circle ------------------------------------------------
+
+import java.util.*;
+
+public class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float area = 3.14f*rad*rad;    // java mein float value ke aage f lagta hai nahi vo double ho jaati hai ....
+        System.out.println(area);
+
+    }
+}
+
+
+
+
+conversion------------------------------------------------------
+
+import java.util.*;
+
+public class input{
+    public static void main(String args[]){
+       int a = 25;
+       long b =a;
+
+    //    long a = 25;
+    //    int b =a;
+
+       System.out.println(b);
+
+    }
+}
+
+
+
+import java.util.*;
+
+public class input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // int number = sc.nextFloat();
+        float number = sc.nextInt();
     
-//         System.out.println(number);
+        System.out.println(number);
 
-//     }
-// }
+    }
+}
 
 
 
