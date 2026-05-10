cf7ac97d224f7f99d5ca5c4b0f46e6aaf2b74b57
sachin555-lab <sachintailor711@gmail.com>
2026-05-10 14:06:06 +0530

    Add Java classes for OddEven, conditional checks, sum of three, switch cases, and ternary operations with examples

---
+++ Day4/sumofthree.java
@@ -0,0 +1,25 @@
+
+// sum of three --------------------------------------------------------------------
+
+import java.util.*;
+public class sumofthree{
+    public static void main(String args[]){
+        int a = 1 , b = 3 , c = 6;
+
+        if((a>=b) && (a>=c)){
+            System.out.println("Largest is a  ");
+            
+        }
+        else if(a>=c){
+            System.out.println("largest is b ");
+            
+        }
+        else{
+            System.out.println("largest is c ");
+        }
+
+    }
+}
+
+
+
---
+++ Day4/tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+mport java.util.*;
+// public class ternary{
+//     public static void main(String args[]){
+
+//         int marks = 95;
+
+//         if(marks>=33){
+//             System.out.println("pass");
+//         }
+        
+//         else{
+//              System.out.println("fail");
+
+//         }
+//     }
+// }
