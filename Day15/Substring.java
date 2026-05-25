eb0dfa11e5daf3bd1593ce7ea7fd59ee743dd9cc
Sachin-Tailor <sachintailor711@gmail.com>
2026-05-25 16:41:03 +0530

    commit

---
+++ Day15/Substring.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+public class Substring{
+    public static String substring(String str, int si, int ei){
+        String substr ="";
+        for(int i=si; i<ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
+
+
+
+    public static void main(String args[]){
+        String str = "Hello world";
+        System.out.println(substring(str,0,5));
+
+    }
+}
+
+
+
