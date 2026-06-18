fc9406cea0238a9c502ad5b4008286f82c6ac2b5
Sachin-Tailor <sachintailor711@gmail.com>
2026-06-18 09:04:58 +0530

    commit

---
+++ Day20/SimpleLoop.java
@@ -0,0 +1,13 @@
+public class SimpleLoop {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+
+        // runs n times -> O(n)
+    }
+}
